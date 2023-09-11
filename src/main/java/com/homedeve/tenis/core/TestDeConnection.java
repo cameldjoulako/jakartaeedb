838dd506acfdbf1fa60c240a7e62ae14a8028820
Camel Djoulako <cameldjoulako17@gmail.com>
2023-09-11 08:00:41 +0100

    java database jdbc

--- src/main/java/com/homedeve/tenis/core/TestDeConnection.java
+++ src/main/java/com/homedeve/tenis/core/TestDeConnection.java
@@ -18,11 +18,47 @@ public class TestDeConnection {
             //Postgres Driver officiel
             //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","homedeve","root");
 
-
+            /*
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT NOM, PRENOM FROM JOUEUR");
 
-            //transmission d'une requette au serveur
+            /transmission d'une requette au serveur
+            ResultSet rs = statement.executeQuery("SELECT NOM, PRENOM, ID FROM JOUEUR");
+
+             while (rs.next()) {
+                final String nom = rs.getString("NOM");
+                final String prenom = rs.getString("PRENOM");
+                final long id = rs.getLong("ID");
+
+                 System.out.println("Joueur" + id + " : " + prenom + " " + nom);
+             }
+
+
+
+             rs.close();
+             statement.close();*/
+
+            //Requettes préparés
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT NOM, PRENOM, ID FROM JOUEUR WHERE ID=?");
+            long identifiant = 78;
+            preparedStatement.setLong(1, identifiant);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if(rs.next()) {
+                final String nom = rs.getString("NOM");
+                final String prenom = rs.getString("PRENOM");
+                final long id = rs.getLong("ID");
+
+                System.out.println("Joueur" + id + " : " + prenom + " " + nom);
+            } else {
+                System.out.println("Il y'a pa d'enregidtrement d'ID 128");
+            }
+
+
+
+
+
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
